e926260695b35a0bf5c0b59930c82a1be0b298d0
JanSmoczynski <73097568+JanSmoczynski@users.noreply.github.com>
2021-03-30 11:03:57 +0200

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonClass singleton = SingletonClass.Instance;
+            singleton.Jest = true;
+            singleton.Nazwa = "Kłamca";
+
+            Console.WriteLine(" Ksiązka pod tytułem  : " + singleton.Nazwa + "\n Czy najduje się w bazie : " + singleton.Jest + "\n");
+
+            SingletonClass singleton2 = SingletonClass.Instance;
+            singleton2.Jest = false;
+
+            Console.WriteLine(" Ksiązka pod tytułem  : " + singleton2.Nazwa + "\n Czy najduje się w bazie : " + singleton2.Jest);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SingletonClass
+    {
+        private static SingletonClass instance = new SingletonClass();
+
+        private bool jest;
+        private string nazwa;
+
+        private SingletonClass() { }
+
+        internal static SingletonClass Instance
+        {
+            get => instance;
+            set => instance = value;
+        }
+
+        public bool Jest { get => jest; set => jest = value; }
+        public string Nazwa { get => nazwa; set => nazwa = value; }
+
+
+    }
+}
